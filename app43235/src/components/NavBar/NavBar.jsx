b706f2d5dc10e2bbc8b0f6551a872b2ce5ac0bd2
fedebeluardo <federico.beluardo@gmail.com>
2023-05-21 21:08:54 -0300

    segundo commit

---
+++ app43235/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>Tienda Drogueria INSA </h3>
+            <div>
+                <button>Insumos médicos</button>
+                <button>Antisepticos</button>
+                <button>Alcoholes</button>
+                <button>Medicamentos</button>
+            </div>
+        </nav>
+    )
+}
+export default NavBar

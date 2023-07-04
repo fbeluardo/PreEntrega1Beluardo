9a3d69252e8066dbfd06f861783d37bc8e6ba5b8
fedebeluardo <federico.beluardo@gmail.com>
2023-07-04 13:32:54 -0300

    version4.7.4

--- app43235/src/services/firebase/firebaseConfig.js
+++ app43235/src/services/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBergng0eHiaRzHsLS-_MlTiL5WoVhoPVw",
-  authDomain: "react-trabajo-final-fb.firebaseapp.com",
-  projectId: "react-trabajo-final-fb",
-  storageBucket: "react-trabajo-final-fb.appspot.com",
-  messagingSenderId: "3525362007",
-  appId: "1:3525362007:web:1b7fe41f5391ff7e7b7e12"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);

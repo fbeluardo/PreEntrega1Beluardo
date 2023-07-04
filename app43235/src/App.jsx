0e677185b891af051a62dcd3ae61c26a8cd5167d
fedebeluardo <federico.beluardo@gmail.com>
2023-07-04 13:53:11 -0300

    version4.7.10

--- app43235/src/App.jsx
+++ app43235/src/App.jsx
@@ -1,6 +1,6 @@
-import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
-import Navbar from "./components/Navbar/Navbar"
+import ItemDetailContainer from "./components/ItemDetailContainer"
+import ItemListContainer from "./components/ItemListContainer"
+import Navbar from "./components/NavBar"
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { CartProvider } from "./components/Context/CartContext"
@@ -15,7 +15,7 @@ function App() {
             <Navbar />
             <Routes>
               <Route path='/' element={<ItemListContainer greeting={'Listado de productos'}/>}/>
-              <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Listado de productos filtrados'}/>}/>
+              <Route path='/categoria/:categoriaId' element={<ItemListContainer greeting={'Listado de productos filtrados'}/>}/>
               <Route path='/item/:itemId' element={<ItemDetailContainer/>}/>
             </Routes>
           </CartProvider>

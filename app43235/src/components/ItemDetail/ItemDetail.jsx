835e6006aab65011ede01aced64367422087176d
fedebeluardo <federico.beluardo@gmail.com>
2023-07-06 14:41:36 -0300

    version6.7.5

--- app43235/src/components/ItemDetail/ItemDetail.jsx
+++ app43235/src/components/ItemDetail/ItemDetail.jsx
@@ -2,14 +2,13 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getDoc, doc, onSnapshot } from 'firebase/firestore';
 import { db } from '../../services/firebase/firebaseConfig';
-import ItemCount from '../ItemCount/ItemCount'
+import ItemCount from '../ItemCount/ItemCount';
 import Checkout from '../Checkout/Checkout';
+import { CartContext } from '../Context/CartContext'
 
 const ItemDetail = () => {
   const [product, setProduct] = useState(null);
   const [stock, setStock] = useState(0);
-  const [cartItems, setCartItems] = useState([]);
-  const [totalPurchase, setTotalPurchase] = useState(0);
   const { itemId } = useParams();
 
   useEffect(() => {
@@ -25,9 +24,9 @@ const ItemDetail = () => {
     return () => unsubscribe();
   }, [itemId]);
 
-  const handleAddToCart = (count) => {
+  const handleAddToCart = (addItem, count) => {
     if (count > stock) {
-      // Prevent adding more items than available stock
+      
       return;
     }
 
@@ -38,8 +37,7 @@ const ItemDetail = () => {
       quantity: count
     };
 
-    setCartItems(prevItems => [...prevItems, itemToAdd]);
-    setTotalPurchase(prevTotal => prevTotal + (product.precio * count));
+    addItem(itemToAdd, count);
     setStock(prevStock => prevStock - count);
   };
 
@@ -48,28 +46,24 @@ const ItemDetail = () => {
   }
 
   return (
-    <div className="item-detail">
-      <img src={product.imagen} alt={product.nombre} />
-      <p>${product.precio}</p>
-      <h1>{product.nombre}</h1>
-      <p>{product.descripcion}</p>
-      {/* Add any other details you want to display */}
-      <ItemCount stock={stock} onAdd={handleAddToCart} />
+    <CartContext.Consumer>
+      {({ addItem }) => (
+        <div className="item-detail">
+          <img src={product.imagen} alt={product.nombre} />
+          <p>${product.precio}</p>
+          <h1>{product.nombre}</h1>
+          <p>{product.descripcion}</p>
+         
+          <ItemCount stock={stock} onAdd={(count) => handleAddToCart(addItem, count)} />
 
-      <div className="cart-items">
-        <h2>Cart Items:</h2>
-        {cartItems.map(item => (
-          <div key={item.id}>
-            <p>Name: {item.name}</p>
-            <p>Price: ${item.price}</p>
-            <p>Quantity: {item.quantity}</p>
+          <div className="cart-items">
+            <h2>Cart Items:</h2>
+            
           </div>
-        ))}
-      </div>
-      <p>Total Purchase: ${totalPurchase}</p>
-
-      <Checkout cartItems={cartItems} totalPurchase={totalPurchase} />
-    </div>
+         
+        </div>
+      )}
+    </CartContext.Consumer>
   );
 };
 

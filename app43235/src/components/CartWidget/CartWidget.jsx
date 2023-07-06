de04ddd5b06d9681589f93a9d1d7d274b66d5529
fedebeluardo <federico.beluardo@gmail.com>
2023-07-06 17:04:52 -0300

    version6.7.5

--- app43235/src/components/CartWidget/CartWidget.jsx
+++ app43235/src/components/CartWidget/CartWidget.jsx
@@ -1,11 +1,18 @@
 import cart from './assets/icons8-shopping-cart-94.png'
+import { useCart } from '../Context/CartContext'
+import { Link } from 'react-router-dom'
+import Cart from '../Cart/Cart'
 
 const CartWidget = () => {
+    const { totalQuantity } = useCart()
+
     return (
         <div>
-            <img src={cart} alt="cart-widget"/>
+            <img src={cart} alt='cart-widget'/>
+            <Link><Cart></Cart></Link>
+            {totalQuantity}
         </div>
-    )    
+    )
 }
 
 export default CartWidget
